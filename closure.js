ca686835442712812f7d3e484cf4baef763faffa
Roketskyi Roman <91413976+Roketskyi@users.noreply.github.com>
2022-12-13 01:01:04 +0200

    Add files via upload

---
+++ closure.js
@@ -0,0 +1,22 @@
+function Plus(n) {
+    return function(num){
+        return `Your result: ${num + n}`;
+    }
+}
+
+const addFive = Plus(5);
+const addTen = Plus(10);
+
+
+function urlSite (domain){
+    return function(url){
+        return `Your Website: https://${url}.${domain}/`;
+    }
+}
+
+const comDomain = urlSite('com');
+const orgDomain = urlSite('org');
+const uaDomain = urlSite('ua');
+
+console.log(addFive(2) + '\n' + addTen(5));
+console.log(comDomain('youtube'))
